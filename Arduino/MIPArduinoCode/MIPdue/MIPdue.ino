921f77a7b49cfd5fd2ae3d8ec2389ea594897875
c.a. Hooft <c.a.hooft@gmail.com>
2018-06-19 13:29:48 +0200

    Added Sound

--- Arduino/MIPArduinoCode/MIPdue/MIPdue.ino
+++ Arduino/MIPArduinoCode/MIPdue/MIPdue.ino
@@ -28,6 +28,31 @@ float THvalue;
 //Declaring some hardware
 Servo myServo;
 
+int length = 26;
+char notes[] = "eeeeeeegcde fffffeeeeddedg";
+int beats[] = { 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2};
+
+int tempo = 300;
+void playTone(int tone, int duration) {
+  for (long i = 0; i < duration * 1000L; i += tone * 2) {
+    digitalWrite(spkr, HIGH);
+    delayMicroseconds(tone);
+    digitalWrite(spkr, LOW);
+    delayMicroseconds(tone);
+  }
+}
+void playNote(char note, int duration) {
+  char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
+  int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
+  
+  // play the tone corresponding to the note name
+  for (int i = 0; i < 8; i++) {
+    if (names[i] == note) {
+      playTone(tones[i], duration);
+    }
+  }
+}
+
 void setup() 
 {
   Serial.begin(9600);
@@ -41,11 +66,23 @@ void setup()
   pinMode(LED2, OUTPUT);
   pinMode(echo, INPUT);
   pinMode(trig, OUTPUT);
+  pinMode(spkr, OUTPUT);
 }
 
 void loop() 
 {
   ReadLight();
+
+  for (int i = 0; i < length; i++) {
+    if (notes[i] == ' ') {
+      delay(beats[i] * tempo); // rest
+    } else {
+      playNote(notes[i], beats[i] * tempo);
+    }
+    
+    // pause between notes
+    delay(tempo / 2); 
+  }
 }
 
 //Method for random movement

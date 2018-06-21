59bb317295e6b01ff187a09472ded3365f219fff
c.a. Hooft <c.a.hooft@gmail.com>
2018-06-21 10:40:17 +0200

    Added basic engie script

---
+++ Arduino/Engines/Engines1.0/Engines1.0.ino
@@ -0,0 +1,27 @@
+int pushButton = 2;
+int motorControl = 9;
+
+void setup() 
+{
+  pinMode(pushButton, INPUT);
+  pinMode(motorControl, OUTPUT);  
+}
+void loop() {
+  if(digitalRead(pushButton) == HIGH)
+  { 
+    // ramp up the motor speed
+    for(int x = 0; x <= 255; x++){
+      analogWrite(motorControl, x);
+      delay(50);
+    }
+
+    // ramp down the motor speed
+    for(int x = 255; x >= 0; x--){
+      analogWrite(motorControl, x);
+      delay(50);
+    }    
+  }
+
+  delay(1);        // delay in between reads for stability
+}
+
